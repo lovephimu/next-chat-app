0b758b8fc2ef28fc736d41b211ceb2aa3bdef9f1
lovephimu <127794781+lovephimu@users.noreply.github.com>
2024-01-20 23:02:56 +0100

    Edit message type

--- migrations/1705508560-insertMessages.ts
+++ migrations/1705508560-insertMessages.ts
@@ -13,17 +13,17 @@ const messages: Message[] = [
   {
     id: 1,
     messageText: 'Awkward silence.',
-    chat_user: 'firefox@127.0.0.1',
+    chatUser: 'firefox@127.0.0.1',
   },
   {
     id: 2,
     messageText: 'Awkward silence yourself.',
-    chat_user: 'chrome@127.0.0.1',
+    chatUser: 'chrome@127.0.0.1',
   },
   {
     id: 3,
     messageText: 'Silence is something nice.',
-    chat_user: 'safari@127.0.0.1',
+    chatUser: 'safari@127.0.0.1',
   },
 ];
 
@@ -33,7 +33,7 @@ export async function up(sql: Sql) {
     INSERT INTO messages
     (message_text, chat_user)
     VALUES
-    (${message.messageText}, ${message.chat_user})
+    (${message.messageText}, ${message.chatUser})
     `;
   }
 }
