c6a100811db93264aab532b5b55d726bee32d491
lovephimu <127794781+lovephimu@users.noreply.github.com>
2024-01-21 11:20:09 +0100

    Add headers position

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,16 @@ import { getUserIp } from '@/util/functions/getUserIp';
 import { headers } from 'next/headers';
 import Chat from './components/Chat';
 
-const header = headers();
-
-// get user IP & user browser name
-
-const ip = getUserIp(header);
-console.log(ip);
-
-const browser = getUserBrowserName(header);
-console.log(browser);
-
 export default function Home() {
+  const header = headers();
+
+  // get user IP & user browser name
+
+  const ip = getUserIp(header);
+  console.log(ip);
+
+  const browser = getUserBrowserName(header);
+  console.log(browser);
   return (
     <main className="text-primaryPink p-4 h-full w-full flex justify-center">
       <Chat ip={ip} browser={browser} />
