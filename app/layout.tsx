b6bd3751ff82a8371f8c9cf2e20c41a5c8c96589
lovephimu <127794781+lovephimu@users.noreply.github.com>
2024-01-24 22:48:00 +0100

    Fix height isses at large screen sizes

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,10 +21,12 @@ export default function RootLayout({
         className={`bg-primaryBlue ${inter.className} dynamic-full-height flex flex-col w-full items-center sm:p-10`}
       >
         <main className="h-[100dvh] max-h-[100dvh] w-full flex flex-col justify-center items-center">
-          <section className="flex h-[20dvh] justify-center w-full sm:justify-start sm:max-w-2xl sm:border-primaryPink sm:border-t sm:border-x sm:rounded-t-2xl">
+          <section className="flex h-[20dvh] justify-center w-full sm:justify-start sm:max-w-2xl sm:border-primaryPink sm:border-t sm:border-x sm:rounded-t-2xl wide-screen-height-logo">
             <ChatLogo />
           </section>
-          <section className="w-full flex justify-center">{children}</section>
+          <section className="w-full flex justify-center h-[80dvh] sm:h-[70dvh] wide-screen-height">
+            {children}
+          </section>
         </main>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Chat from './components/Chat';
 
 export default function Home() {
   return (
-    <section className="text-primaryPink  h-full w-full flex justify-center sm:max-w-2xl sm:border-b-4 sm:border-x sm:rounded-b-2xl sm:border-primaryPink">
+    <section className="text-primaryPink h-full w-full flex justify-center sm:max-w-2xl sm:border-b-8 sm:border-x sm:rounded-b-2xl sm:border-primaryPink">
       <Chat />
     </section>
   );
