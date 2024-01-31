799c5765ed99aba1aa2a155b0cd1c416a6d958fb
lovephimu <127794781+lovephimu@users.noreply.github.com>
2024-01-31 19:28:51 +0100

    Edit title of visitors page

--- app/visitors/page.tsx
+++ app/visitors/page.tsx
@@ -1,4 +1,5 @@
 import { countUsers } from '@/util/functions/countUsers';
+import { globalMessageLimit } from '@/util/variables/globalVariables';
 import ChatUsers from '../components/ChatUsers';
 
 export default async function VisitorsPage() {
@@ -23,7 +24,11 @@ export default async function VisitorsPage() {
   return (
     <section className="text-primaryPink h-full w-full flex justify-center sm:max-w-2xl relative">
       <div className="text-primaryPink h-full w-full flex flex-col justify-center sm:max-w-2xl sm:border-b-8 sm:border-x sm:rounded-b-2xl sm:border-primaryPink items-center">
-        <h3>Three most chatty browsers by message:</h3>
+        {countedUsers.length > 1 ? (
+          <h3>Most chatty browsers of the last 5 messages:</h3>
+        ) : (
+          <h3>The most chatty browser of the last 5 messages:</h3>
+        )}
         <ChatUsers agents={countedUsers} />
       </div>
     </section>
